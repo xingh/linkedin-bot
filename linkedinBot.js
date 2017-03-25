07f8170c4a99ff793b3ecdeb464c7146f6cb3c09
Rahul Singh <rahul.singh@anant.us>
2017-03-25 13:42:52 -0400

    Commenting connect click

--- linkedinBot.js
+++ linkedinBot.js
@@ -14,13 +14,17 @@ function sendRequest(){
 	var contactsNum = 0;
 	jQuery.each( $('li.mn-person-card'), 	function() {
 		contactsNum++;
-		//$(this).click();
+
 		var identity_full_name = $(this).find('a.mn-person-info__link span.mn-person-info__name').text().trim();
 		var identity_occupation = $(this).find('a.mn-person-info__link span.mn-person-info__occupation').text().trim();
 		var identity_occupation_split = identity_occupation.split(' at '); 
 		var identity_occupation_title = identity_occupation_split[0];
 		var identity_occupation_company = identity_occupation_split[1];
 		var identity_social_lin = 'https://linkedin.com'+$(this).find('a.mn-person-info__link').attr('href');
+		
+		//IMPORTANT - UNCOMMENT THIS LINE IF YOU WANT TO CONNECT PEOPLE. I DONT TURN THIS ON BY DEFAULT BECAUSE IT SHOULD BE USED CAREFULLY
+		//$(this).find('div.mn-person-card__card-actions button.mn-person-card__person-btn-ext.button-secondary-medium').click();
+		
 		console.log(contactsNum+','+identity_full_name+','+identity_occupation_title+','+identity_occupation_company+','+identity_social_lin);
 	});
 	console.log('Just added contacts: ' + contactsNum);
