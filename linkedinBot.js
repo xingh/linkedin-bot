e4f1ed51d07aefc9f98e43ee24eb8862aed99647
Rahul Singh <rahul.singh@anant.us>
2017-03-29 20:46:48 -0400

    collapsing send / request mehtod
    turns out send/accept is the same css selector so the same
    logic works, and the emberview for a person has the same data.

--- linkedinBot.js
+++ linkedinBot.js
@@ -7,13 +7,13 @@ function scrollDown(height, countOfPagesScrolled){
 		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
 			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
 		}else
-			//return sendRequest();
-			return acceptRequest();
+			return sendOrAcceptRequest();
 	}, 1500);
 }
 
 // Execute this (commented) if on My Network https://www.linkedin.com/mynetwork/
-function sendRequest(){
+// Execute this (commented) if on Invitation Manager : https://www.linkedin.com/mynetwork/invitation-manager/
+function sendOrAcceptRequest(){
 	var contactsNum = 0;
 	jQuery.each( $('li.mn-person-card'), 	function() {
 		contactsNum++;
@@ -33,25 +33,5 @@ function sendRequest(){
 	console.log('Just added contacts: ' + contactsNum);
 }
 
-// Execute this (commented) if on Invitation Manager : https://www.linkedin.com/mynetwork/invitation-manager/
-function acceptRequest(){
-	var contactsNum = 0;
-	jQuery.each( $('li.mn-person-card'), 	function() {
-		contactsNum++;
-
-		var identity_full_name = $(this).find('a.mn-person-info__link span.mn-person-info__name').text().trim();
-		var identity_occupation = $(this).find('a.mn-person-info__link span.mn-person-info__occupation').text().trim();
-		var identity_occupation_split = identity_occupation.split(' at '); 
-		var identity_occupation_title = identity_occupation_split[0];
-		var identity_occupation_company = identity_occupation_split[1];
-		var identity_social_lin = 'https://linkedin.com'+$(this).find('a.mn-person-info__link').attr('href');
-		
-		//IMPORTANT - UNCOMMENT THIS LINE IF YOU WANT TO CONNECT PEOPLE. I DONT TURN THIS ON BY DEFAULT BECAUSE IT SHOULD BE USED CAREFULLY
-		//$(this).find('div.mn-person-card__card-actions button.mn-person-card__person-btn-ext.button-secondary-medium').click();
-		
-		console.log(contactsNum+','+identity_full_name+','+identity_occupation_title+','+identity_occupation_company+','+identity_social_lin);
-	});
-	console.log('Just accepted invitations: ' + contactsNum);
-}
 
 scrollDown(document.body.clientHeight, countOfPagesScrolled);
