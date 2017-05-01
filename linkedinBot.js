7e820263338975f696e28c7a587d2b3fddcb15f9
Rahul Singh <rahul.singh@anant.us>
2017-05-01 11:17:24 -0400

    Adding Double Quotes for Better CSV Parsing Later
    Adding Double Quotes for Better CSV Parsing Later in Excel / Google Spreadsheet.

--- linkedinBot.js
+++ linkedinBot.js
@@ -1,5 +1,5 @@
 //TODO : Change this to the number of pages to scroll. There are about 10-15 people per page. Don't go over board. 
-var countOfPagesScrolled = 5;
+var countOfPagesScrolled = 3;
 
 function scrollDown(height, countOfPagesScrolled){
 	scroll(0, document.body.clientHeight);
@@ -28,7 +28,7 @@ function sendOrAcceptRequest(){
 		//IMPORTANT - UNCOMMENT THIS LINE IF YOU WANT TO CONNECT PEOPLE. I DONT TURN THIS ON BY DEFAULT BECAUSE IT SHOULD BE USED CAREFULLY
 		//$(this).find('div.mn-person-card__card-actions button.mn-person-card__person-btn-ext.button-secondary-medium').click();
 		
-		console.log(contactsNum+','+identity_full_name+','+identity_occupation_title+','+identity_occupation_company+','+identity_social_lin);
+		console.log(contactsNum+',"'+identity_full_name+'","'+identity_occupation_title+'","'+identity_occupation_company+'","'+identity_social_lin+'"');
 	});
 	console.log('Just added contacts: ' + contactsNum);
 }
